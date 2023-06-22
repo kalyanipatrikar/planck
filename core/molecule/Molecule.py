cb45ba9cf86b2d84f152b95d8601b84e7bdc86b9
Hemanth Haridas <hemanthhari23@gmail.com>
2023-06-22 22:37:38 +0530

    need to test Molecule.py

--- core/molecule/Molecule.py
+++ core/molecule/Molecule.py
@@ -1,6 +1,7 @@
 import  numpy
 import  typing  
 import  scipy.linalg
+import  os
 from    ..molecule import PeriodicTable
 
 class Molecule:
@@ -15,11 +16,13 @@ class Molecule:
     self.shells       = []
 
   def read_basis(self):
-    atomic_numbers     = [PeriodicTable.atom_maps[row[0]] for row in self.geometry]
+    atomic_numbers    = [PeriodicTable.atom_maps[row[0]] for row in self.geometry]
+    object_path       = os.path.dirname(os.path.dirname(__file__))
     for atom in atomic_numbers:
-      with open("../basissets/{}/{}-{}.txt".format(self.basis_set, self.basis_set, atom)) as basis_object:
+      basis_file      = "/basissets/{}/{}-{}.txt".format(self.basis_set, self.basis_set, atom)
+      basis_path      = object_path+basis_file
+      with open(basis_path) as basis_object:
         basis_data  = basis_object.readlines()
-        print(basis_data)
         for lnumber, line in enumerate(basis_data[1:]):
           if "S" in line and "P" not in line:
               nprims          =   int(line.split()[1])
@@ -35,18 +38,12 @@ class Molecule:
           if "P" in line:
               nprims          =   int(line.split()[1])
               pgtodata        =   [x.replace('D', 'E').split() for x in basis_data[lnumber+2:lnumber+2+nprims]]
-              coefficient1    =   [float(x[1]) for x in pgtodata]
-              coefficient2    =   [float(x[2]) for x in pgtodata]
+              coefficients    =    [float(x[1]) for x in pgtodata]
               exponents       =   [float(x[0]) for x in pgtodata]
-              shell00         =   [0, 0, 0]
               shell11         =   [1, 0, 0]
               shell12         =   [0, 1, 0]
               shell13         =   [0, 0, 1]
             
-              self.shells.append(shell00)
-              self.exponents.append(exponents)
-              self.coefficients.append(coefficients)
-
               self.shells.append(shell11)
               self.exponents.append(exponents)
               self.coefficients.append(coefficients)
